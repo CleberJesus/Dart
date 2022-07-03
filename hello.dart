fb8934c9d17f24cede8dc8fe2b4da76ff1232067
CleberJesus <spiritlion.co@hotmail.com>
2022-07-03 12:49:54 -0300

    operadores aritiméticos

--- hello.dart
+++ hello.dart
@@ -3,6 +3,12 @@ main(){
   print("segunda impressão!");
   print(2022);
   print("dia 03");
-  int d = 03;
+  int d = 3;
   print(d);
+
+  var hello = "hello word 2";
+
+  print("$hello $d");
+ 
+
 }
